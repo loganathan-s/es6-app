a370c936de283812e7c03f9958378ec2ca93eef4
Loganathan Sellappa <loganathan.ms@gmail.com>
2017-04-25 01:12:20 +0530

    Sets & weakSet sample codes added

---
+++ src/sets.js
@@ -0,0 +1,30 @@
+const people = new Set();
+people.add("Logan");
+people.add("Eliza");
+//Delete element from set
+people.delete("Logan")
+
+//clear whole set
+people.clear
+
+//se is always has uniq data
+
+
+ const people = new Set();
+  people.add('Wes');
+  people.add('Snickers');
+  people.add('Kait');
+  for (const person of  people) {
+    console.log(person);
+  }
+  const students = new Set(['Wes', 'Kara', 'Tony']);
+  const dogs = ['Snickers', 'Sunny'];
+  const dogSet = new Set(dogs);
+
+
+//WeakSet  can contain only object, we cannot loop, iterator on weakset
+
+let one = "logan";
+let two = "two";
+
+const weak_set = new WeakSet([one, two]); //weak set accept only object and it will clear the element from the set whenever the object is reomved from the code (i.e Garbagecollerctor)
